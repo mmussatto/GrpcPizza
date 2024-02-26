a292611206358ab384694a74ff53ca1b1678fd76
mmussatto <murilomussatto@gmail.com>
2024-02-26 18:20:30 -0300

    Closes #6; Add Pizza Context

---
+++ PizzaCatalog/Data/PizzaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaCatalog.Models;
+
+namespace PizzaCatalog;
+
+public class PizzaContext : DbContext
+{
+    public PizzaContext(DbContextOptions options)
+        : base(options) { }
+
+    public DbSet<Pizza>? Pizzas { get; set; }
+}
