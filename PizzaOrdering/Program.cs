406a8142a10a11a608ca4d6b422ab8b767859e9c
mmussatto <murilomussatto@gmail.com>
2024-02-27 08:58:51 -0300

    Closes #10 - Setup Postgres Database Connection

--- PizzaOrdering/Program.cs
+++ PizzaOrdering/Program.cs
@@ -1,5 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaOrdering.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<OrderContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("OrderContext"))
+);
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<OrderContext>();
+        var created = context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
 
 app.MapGet("/", () => "Hello World!");
 
