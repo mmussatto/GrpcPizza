4742ce308b9abb86fdd1f36dab805c4baacd733b
mmussatto <murilomussatto@gmail.com>
2024-02-27 08:53:17 -0300

    Closes #8 - Create Order Model

---
+++ PizzaOrdering/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaOrdering.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderDateTime { get; set; } = DateTime.Now;
+    public int PizzaId { get; set; }
+    public bool Done { get; set; } = false;
+}
